fa885e3b62f205ad7eeec64d311c86a35e4cab7a
adipa <adipa.achintha@gmail.com>
2015-08-09 15:53:43 +1000

    fixes to the site

---
+++ angular-forms-validator/app/controller/allValidatorsController.js
@@ -0,0 +1,6 @@
+﻿(function() {
+    angular.module('validatorApp').controller('allValidators', ['$scope', function ($scope) {
+        $scope.requiredVal = { enabled: true, message: 'Required validation', value: '' };
+        $scope.stringVal = { enabled: true, message: 'String validation', minLength : 2,maxLength : 5 , value: '' };
+	}]);
+})();
---
+++ angular-forms-validator/app/controller/introVariableController.js
@@ -0,0 +1,5 @@
+﻿(function() {
+    angular.module('validatorApp').controller('introVariable', ['$scope', function ($scope) {
+        $scope.model = { requiredField: '', enableValidation: true, validationMessage : 'Test Message' };
+	}]);
+})();
